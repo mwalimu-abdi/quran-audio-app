89ac9f7b16588193193becb0a88bf568440a788a
mwalimu-abdi <abdicool622@gmail.com>
2025-12-26 12:12:21 +0300

    Add BrowserRouter with basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";  // ✅ Import BrowserRouter
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/quran-audio-app">  {/* ✅ Set basename to your repo name */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
